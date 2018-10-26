2ff8810919dde72b4acc7218896a6720859d6f17
DeBoevereBen <ben.de.boevere@student.howest.be>
2018-10-26 09:20:42 +0200

    User page for admins

--- src/components/Users.vue
+++ src/components/Users.vue
@@ -6,8 +6,27 @@
           </v-flex>
       </v-layout>
       <v-layout row wrap>
-          <v-flex xs4 offset-xs1 ref="results" @>
-            <searchbar @select-item="applySelection" :list="gebruikers" :concat_keys="keys" :labeltext="zoeklabel" :item_concat_key="item_name" :item_value="item_value"></searchbar>
+          <v-flex row ref="results" @>
+              <v-text-field type="text" placeholder="Filter op naam" v-model="nameFilter" v-on:input="applyFilters()"></v-text-field>
+              <v-select
+                :items="roles"
+                v-model="roleFilter"
+                label="Filter op rol"
+                v-on:input="applyFilters()"
+              ></v-select>
+              <v-select
+                :items="genders"
+                v-model="genderFilter"
+                label="Filter op geslacht"
+                v-on:input="applyFilters()"
+              ></v-select>
+              <v-select
+                v-model="statusFilter"
+                label="Filter op status"
+                :items="statusses"
+                v-on:input="applyFilters()"
+              >
+              </v-select>
           </v-flex>
           <v-flex xs1 offset-xs4 class="mr-5">
           </v-flex>
@@ -47,26 +66,27 @@
           </v-speed-dial>
       </v-layout>
       <v-layout row wrap>
-          <v-flex class="mt-4" offset-xs1 xs2>
-            <checkboxes :listobject="opleidingen" v-if="receivedData" @update-filters="applyFilters"></checkboxes>
-          </v-flex>
-          <v-flex class="mt-4" xs8>
+          <v-flex class="mt-4">
             <v-data-table
             v-bind:headers="headers"
-            :items="gebruikers"
+            :items="filteredGebruikers"
             hide-actions
             class="elevation-1"
             >
               <template slot="items" slot-scope="gebruiker"><!-- .item must be here, don't ask questions -->
                 <tr >
-                <td class="text-xs-left">{{ gebruiker.item.firstname + ' ' + gebruiker.item.lastname  }}</td>
-                <td class="text-xs-left">{{ gebruiker.item.opleidingsNaam }}</td>
-                <td>
+                <td class="text-xs-left">{{ gebruiker.item.naam }}</td>
+                <td class="text-xs-left">{{ getKeyByValue(roleKeys, gebruiker.item.role) }}</td>
+                <td class="text-xs-left">{{ gebruiker.item.email }}</td>
+                <td class="text-xs-left">{{ gebruiker.item.gender }}</td>
+                <td class="text-xs-left">{{ gebruiker.item.status }}</td>
+                <td class="text-xs-left">{{ readableDate(gebruiker.item.accountCreatedTimestamp) }}</td>
+                <!-- <td>
                     <a :href="'#/Evaluate?id=' + gebruiker.item.id" target="_blank"><v-btn color="primary" class="ma-1" dark>Evaluatie<v-icon dark right>assignment</v-icon></v-btn></a>
-                    <router-link :to="{ path: 'Rapporten', query: { id: gebruiker.item.id, name: gebruiker.item.firstname + ' ' + gebruiker.item.lastname }}"><v-btn color="primary" class="ma-1" dark>rapport<v-icon dark right>import_contacts</v-icon></v-btn></router-link>
+                    <router-link :to="{ path: 'Rapporten', query: { id: gebruiker.item.id, name: gebruiker.item.naam}}"><v-btn color="primary" class="ma-1" dark>rapport<v-icon dark right>import_contacts</v-icon></v-btn></router-link>
                     <router-link :to="{ path: 'Addstudent', query: { id: gebruiker.item.id }}"><v-btn color="primary" class="ma-1" dark><v-icon dark>edit</v-icon></v-btn></router-link>
                     <v-btn color="error" class="ma-1" dark @click="makeDialog(gebruiker.item)"><v-icon dark>delete</v-icon></v-btn>
-                </td>
+                </td> -->
               </tr>
               </template>
             </v-data-table>
@@ -132,6 +152,7 @@
 
 <script>
 import * as api from "../services/organizer-api";
+import * as _ from "lodash";
 
 function createStudentOpleidingMap(students, opleidingen) {
   return students.map(function(student) {
@@ -142,6 +163,11 @@ function createStudentOpleidingMap(students, opleidingen) {
   });
 }
 
+function addLeadingZero(numberToFormat) {
+  return (numberToFormat =
+    numberToFormat < 10 ? "0" + numberToFormat : numberToFormat);
+}
+
 export default {
   name: "Gebruikers",
   data() {
@@ -150,6 +176,7 @@ export default {
       receivedData: false,
       formData: [],
       gebruikers: [],
+      filteredGebruikers: [],
       addStudentsFile: false,
       search: "",
       keys: ["firstname", "lastname"],
@@ -158,15 +185,49 @@ export default {
       item_name: "fullname",
       item_value: "id",
       headers: [
-        { text: "Student", align: "left", value: "student" },
-        { text: "Opleiding", align: "left", value: "opleiding" },
-        { text: "", value: "rapportid" }
+        { text: "Naam", align: "left", value: "naam", sortable: true },
+        { text: "Rol", align: "left", value: "role", sortable: true },
+        { text: "Email", align: "left", value: "email", sortable: true },
+        { text: "Geslacht", align: "left", value: "gender", sortable: true },
+        { text: "Status", align: "left", value: "status", sortable: true },
+        { text: "Datum aangemaakt", value: "accountCreatedTimestamp" }
       ],
       dialogUser: {
         firstname: "",
         lastname: "",
         userId: "",
         show: false
+      },
+      nameFilter: "",
+      roleFilter: "",
+      genderFilter: "",
+      statusFilter: "Actief",
+      roleKeys: {
+        "Geen filter": false,
+        Administrator: "ADMIN",
+        Student: "STUDENT",
+        Leerkracht: "TEACHER",
+        "Geen rol toegekent": "#"
+      },
+      roles: [
+        "Geen filter",
+        "Administrator",
+        "Student",
+        "Leerkracht",
+        "Geen rol toegekent"
+      ],
+      genders: ["Geen filter", "M", "V"],
+      genderKeys: {
+        "Geen filter": false,
+        M: "M",
+        V: "F"
+      },
+      statusses: ["Geen filter", "Actief", "Wacht op activatie", "Niet actief"],
+      statussesKeys: {
+        "Geen filter": false,
+        Actief: "ACTIVE",
+        "Wacht op activatie": "AWAIT_ACTIVATION",
+        "Niet actief": "DISABLES"
       }
     };
   },
@@ -181,30 +242,40 @@ export default {
       const form = this.formData;
       console.log(form);
     },
-    applyFilters(payload) {
-      var self = this;
-      payload.forEach(function(filter) {
-        if (self.filters.includes(filter.opleiding)) {
-          if (filter.value === true) {
-            const index = self.filters.indexOf(filter.opleiding);
-            self.filters.splice(index, 1);
+    applyFilters() {
+      const naamFiltertje = this.nameFilter.toLowerCase();
+      const rolFiltertje = this.roleKeys[this.roleFilter]
+        ? this.roleKeys[this.roleFilter].toLowerCase()
+        : false;
+      const genderFiltertje = this.genderKeys[this.genderFilter];
+      const statusFiltertje = this.statussesKeys[this.statusFilter];
+      console.log(naamFiltertje, rolFiltertje, genderFiltertje);
+      this.filteredGebruikers = this.gebruikers
+        .filter(x => {
+          console.log(x.gender, genderFiltertje);
+          if (!genderFiltertje) {
+            return true;
           }
-        } else if (filter.value === false) {
-          self.filters.push(filter.opleiding);
-        }
-      });
-    },
-    applySelection(payload) {
-      this.selectedid = payload;
-    },
-    checkSelected(id) {
-      if (this.selectedid === null) {
-        return true;
-      } else if (this.selectedid === id) {
-        return true;
-      } else {
-        return false;
-      }
+          return x.gender === genderFiltertje;
+        })
+        .filter(x => {
+          if (!rolFiltertje) {
+            return true;
+          }
+          const roleToCheck = x.role ? x.role : "#";
+          return roleToCheck.toLowerCase() === rolFiltertje;
+        })
+        .filter(x => {
+          return `${x.firstname} ${x.lastname}`
+            .toLowerCase()
+            .includes(naamFiltertje);
+        })
+        .filter(x => {
+          if (!statusFiltertje) {
+            return true;
+          }
+          return x.status === statusFiltertje;
+        });
     },
     deleteUser(id) {
       console.log(`Gebruiker met id: ${id} wordt verwijderd`);
@@ -212,12 +283,31 @@ export default {
       this.dialogUser.lastname = "";
       this.dialogUser.userId = "";
       this.dialogUser.show = false;
+    },
+    readableDate(timeStamp) {
+      const stamp = timeStamp.split("T");
+      const date = stamp[0].split("-");
+      const time = stamp[1].split(":");
+      console.log(date, time);
+      let day = date[2];
+      let month = date[1];
+      const year = date[0];
+      let hour = time[0];
+      let minutes = time[1];
+      return `${day}/${month}/${year}, ${hour}:${minutes}`;
+    },
+    getKeyByValue(object, value) {
+      return Object.keys(object).find(key => object[key] === value);
     }
   },
   async created() {
     const gebruikers = await this.$http.getUsers();
     this.gebruikers = gebruikers;
-    console.log(this.gebruikers);
+    for (const gebruiker of this.gebruikers) {
+      gebruiker.naam = `${gebruiker.firstname} ${gebruiker.lastname}`;
+    }
+    this.filteredGebruikers = this.gebruikers;
+    this.applyFilters();
   },
   mounted() {
     var sheet = document.createElement("style");
