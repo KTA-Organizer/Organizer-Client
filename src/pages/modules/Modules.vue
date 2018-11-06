9b30ec4b401ece98346d0b3110882ae7410810a5
VandendriesscheWard <ward.vandendriessche@student.howest.be>
2018-11-06 16:14:11 +0100

    Able to edit module name

--- src/pages/modules/Modules.vue
+++ src/pages/modules/Modules.vue
@@ -1,19 +1,25 @@
 <template>
 <main>
-    
-    <v-layout row class="ml-5">
-        <v-flex xs4>
-            <v-text-field name="modulenaam" label="Naam van de opleiding" v-model="modulenaam" single-line></v-text-field>
-        </v-flex>
-        <v-flex xs5>
-            <v-btn color="success" :loading="loading" @click.native="saveOpleiding" :disabled="loading">
-                Opslaan
-                <span slot="loader" class="custom-loader">
-                  <v-icon light>cached</v-icon>
-                </span>
-            </v-btn>
+    <v-layout row-wrap>
+        <v-flex xs12 offset-xs1 class="text-xs-left">
+            <h1 class="display-3">{{ 'Modules' }}</h1>
         </v-flex>
     </v-layout>
+    <v-container>
+        <v-layout row class="ml-5">
+            <v-flex xs4>
+                <v-text-field name="modulenaam" label="Naam van de module" v-model="module.name" single-line></v-text-field>
+            </v-flex>
+            <v-flex xs5>
+                <v-btn color="success" @click.native="saveModule">
+                    Opslaan
+                    <span slot="loader" class="custom-loader">
+                    <v-icon light>cached</v-icon>
+                    </span>
+                </v-btn>
+            </v-flex>
+        </v-layout>
+    </v-container>
 </main>
 </template>
 
@@ -22,70 +28,29 @@ export default {
     name: "Modules",
     data() {
         return {
-            module: null
+            module: null,
+            currentUserId: this.$store.getters.currentUser.id
         };
     },
     methods: {
-        
+        saveModule(){
+            var self = this;
+            self.$http.updateModule(
+                        this.module.id,
+                        this.module.name
+                    );
+        }
     },
     async created() {
         var self = this;
         this.module = await this.$http.getModule(parseInt(this.$route.query.id));
         console.log(module);
-    },
-    mounted() {
-        var sheet = document.createElement("style");
-        sheet.innerHTML = "div.menu__content--autocomplete {top:165px !important;}";
-        document.body.appendChild(sheet);
     }
 };
 </script>
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 
 <style scoped>
-.custom-loader {
-    animation: loader 1s infinite;
-    display: flex;
-}
-
-@-moz-keyframes loader {
-    from {
-        transform: rotate(0);
-    }
-
-    to {
-        transform: rotate(360deg);
-    }
-}
-
-@-webkit-keyframes loader {
-    from {
-        transform: rotate(0);
-    }
-
-    to {
-        transform: rotate(360deg);
-    }
-}
-
-@-o-keyframes loader {
-    from {
-        transform: rotate(0);
-    }
-
-    to {
-        transform: rotate(360deg);
-    }
-}
-
-@keyframes loader {
-    from {
-        transform: rotate(0);
-    }
-
-    to {
-        transform: rotate(360deg);
-    }
-}
+div.menu__content--autocomplete {top:165px !important;}
 </style>
 
