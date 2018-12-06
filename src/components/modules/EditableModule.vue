5178300b58a1485500f2ef8c62c2ca8853a73eec
DeBoevereBen <ben.de.boevere@student.howest.be>
2018-12-06 12:46:23 +0100

    fixed show when editing modules & check on active

--- src/components/modules/EditableModule.vue
+++ src/components/modules/EditableModule.vue
@@ -1,58 +1,67 @@
 <template>
-    <v-container>
-        <v-layout row class="ml-5 mb-4">
-            <v-flex xs4>
-                <v-text-field name="modulenaam" label="Naam module" v-model="module.name" single-line></v-text-field>
-            </v-flex>
-            <v-flex xs5>
-                <v-btn color="success" @click="saveModule">
-                    Opslaan
-                    <span slot="loader" class="custom-loader">
+<v-container>
+    <v-layout row class="ml-5 mb-4">
+        <v-flex xs4>
+            <v-text-field name="modulenaam" label="Naam module" v-model="module.name" single-line></v-text-field>
+        </v-flex>
+        <v-flex xs5>
+            <v-btn color="success" @click="saveModule">
+                Opslaan
+                <span slot="loader" class="custom-loader">
                     <v-icon light>cached</v-icon>
                     </span>
-                </v-btn>
-                <v-btn color="primary" @click="$emit('confirm'), $emit('update:edit', !edit)">Return</v-btn>
-            </v-flex>
-            
-        </v-layout>
-        <v-layout wrap class="ml-5" dark v-for="(categorie) in module.domains" :value="categorie.active" v-bind:key="categorie.name">
-            <h2 class="categorieTitle text-xs-left" v-if="categorie.active"> <v-text-field name="categorienaam" label="Naam categorie" @change="addUpdateAddition('domain',categorie.id)" v-model="domainMap[categorie.id]" single-line ></v-text-field></h2>
-            <v-data-table hide-headers :items="categorie.goals" hide-actions class="elevation-1 criteriaTable">
-                <template slot="items" slot-scope="props" v-if="props.item.active">
-                    <tr>
-                        <th><v-text-field name="doelstellingnaam" label="Naam doelstelling" @change="addUpdateAddition('goal',props.item.id)" v-model="goalMap[props.item.id]" single-line></v-text-field></th>
-                        <v-flex d-flex xs48 sm24 md12>
-                            <v-layout row wrap>
-                                <v-flex d-flex>
-                                    <v-layout row wrap>
-                                        <v-flex  v-for="(criteria) in props.item.criteria" v-bind:key="criteria.name" d-flex xs12 >
-                                            <div v-if="criteria.active" class="fullWidth"><td class="text-xs-left oneThirdWidth"><v-text-field name="criterianaam" label="Naam criteria" @change="addUpdateAddition('criteria',criteria.id)" v-model="criteriaMap[criteria.id]" single-line></v-text-field></td></div>
-                                            <v-divider></v-divider>
-                                        </v-flex>
-                                    </v-layout>
-                                </v-flex>
-                            </v-layout>
-                        </v-flex>
-                    </tr>
-                </template>
-            </v-data-table>
-        </v-layout>
+            </v-btn>
+            <v-btn color="primary" @click="$emit('confirm'), $emit('update:edit', !edit)">Return</v-btn>
+        </v-flex>
 
-    </v-container>
+    </v-layout>
+    <v-layout wrap class="ml-5" dark v-for="(categorie) in module.domains" v-if="categorie.active" :value="categorie.active" v-bind:key="categorie.name">
+        <h2 class="categorieTitle text-xs-left" v-if="categorie.active">
+            <v-text-field name="categorienaam" label="Naam categorie" @change="addUpdateAddition('domain',categorie.id)" v-model="domainMap[categorie.id]" single-line></v-text-field>
+        </h2>
+        <v-data-table hide-headers :items="categorie.goals" hide-actions class="elevation-1 criteriaTable">
+            <template slot="items" slot-scope="props" v-if="props.item.active">
+                <tr>
+                    <th>
+                        <v-text-field :multi-line="true" name="doelstellingnaam" label="Naam doelstelling" @change="addUpdateAddition('goal',props.item.id)" v-model="goalMap[props.item.id]" single-line></v-text-field>
+                    </th>
+                    <v-flex d-flex xs48 sm24 md12>
+                        <v-layout row wrap>
+                            <v-flex d-flex>
+                                <v-layout row wrap>
+                                    <v-flex v-for="(criteria) in props.item.criteria" v-bind:key="criteria.name" d-flex xs12>
+                                        <div v-if="criteria.active" class="fullWidth">
+                                            <td class="text-xs-left oneThirdWidth">
+                                                <v-text-field name="criterianaam" label="Naam criteria" @change="addUpdateAddition('criteria',criteria.id)" v-model="criteriaMap[criteria.id]" single-line></v-text-field>
+                                            </td>
+                                        </div>
+                                        <v-divider></v-divider>
+                                    </v-flex>
+                                </v-layout>
+                            </v-flex>
+                        </v-layout>
+                    </v-flex>
+                </tr>
+            </template>
+        </v-data-table>
+    </v-layout>
+
+</v-container>
 </template>
+
 <script>
 export default {
     name: "editableModule",
     props: ["module", "edit"],
-    data(){
-      return{
-          domainMap: {},
-          goalMap: {},
-          criteriaMap: {},
-          updates: [],
-          allGoals: [],
-          allCriteria: []
-      }  
+    data() {
+        return {
+            domainMap: {},
+            goalMap: {},
+            criteriaMap: {},
+            updates: [],
+            allGoals: [],
+            allCriteria: []
+        }
     },
     methods: {
         saveModule() {
@@ -63,13 +72,16 @@ export default {
             );
             this.sendUpdates();
         },
-        addUpdateAddition(type, id){
-            this.updates.push({"type": type, "id": id});
+        addUpdateAddition(type, id) {
+            this.updates.push({
+                "type": type,
+                "id": id
+            });
         },
-        sendUpdates(){
+        sendUpdates() {
             var self = this;
             this.updates.forEach(update => {
-                switch(update.type){
+                switch (update.type) {
                     case "domain":
                         self.$http.updateDomain(update.id, this.domainMap[update.id]);
                         break;
@@ -83,12 +95,14 @@ export default {
             })
             this.updates = [];
         },
-        initializeDomainMap(){
+        initializeDomainMap() {
             this.domainMap = this.module.domains.filter(x => x.active === 1).reduce((agg, val) => {
-                return Object.assign(agg, { [val.id]: val.name })
+                return Object.assign(agg, {
+                    [val.id]: val.name
+                })
             }, {});
         },
-        setAllGoalsAndCriteria(){
+        setAllGoalsAndCriteria() {
             this.module.domains.forEach(element => {
                 element.goals.forEach(goal => {
                     this.allGoals.push(goal);
@@ -100,34 +114,39 @@ export default {
             this.initializeGoalMap();
             this.initializeCriteriaMap();
         },
-        initializeGoalMap(){
+        initializeGoalMap() {
             this.goalMap = this.allGoals.filter(goal => goal.active === 1).reduce((agg, val) => {
-                return Object.assign(agg, { [val.id]: val.name })
+                return Object.assign(agg, {
+                    [val.id]: val.name
+                })
             }, {});
         },
-        initializeCriteriaMap(){
+        initializeCriteriaMap() {
             this.criteriaMap = this.allCriteria.filter(criterion => criterion.active === 1).reduce((agg, val) => {
-                return Object.assign(agg, { [val.id]: val.name })
+                return Object.assign(agg, {
+                    [val.id]: val.name
+                })
             }, {});
         }
     },
     async created() {
         this.initializeDomainMap();
         this.setAllGoalsAndCriteria();
-  }
+    }
 }
 </script>
 
 <style scoped>
-    .categorieTitle{
-        width: 75%;
-         margin-top: 3%;
-    }
-    .fullWidth{
-        width: 100%;
-    }
-    .oneThirdWidth{
-        width: 35%;
-    }
-</style>
+.categorieTitle {
+    width: 75%;
+    margin-top: 3%;
+}
 
+.fullWidth {
+    width: 100%;
+}
+
+.oneThirdWidth {
+    width: 35%;
+}
+</style>
--- src/components/modules/ModuleList.vue
+++ src/components/modules/ModuleList.vue
@@ -1,74 +1,74 @@
 <template>
 <v-layout column>
-  <v-card class="mb-4" v-for="(categorie) in module.domains" :value="categorie.active" v-bind:key="categorie.name">
-    <v-card-title primary-title v-if="categorie.active">
-      <h3 class="headline">{{ categorie.name }}</h3>
-    </v-card-title>
-    <v-card-text v-if="categorie.active">
-      <v-data-table hide-headers :items="categorie.goals" hide-actions class="criteriaTable">
-        <template slot="items" slot-scope="props">
-          <tr v-if="props.item.active">
-            <th class="oneThirdWidth">{{ props.item.name }}</th>
-            <div v-for="(criteria) in props.item.criteria" v-bind:key="criteria.name">
-              <v-layout v-if="criteria.active" class="py-2 mt-2" row>
-                <v-layout :class="getClass">
-                  <v-flex>
-                    <td d-block class="text-xs-left pl-0 xs-5 oneThirdWidth">{{ criteria.name }}</td>
-                    <v-divider></v-divider>
-                  </v-flex>
-                </v-layout>
-                <template>
+    <v-card class="mb-4" v-for="(categorie) in module.domains" :value="categorie.active" v-bind:key="categorie.name">
+        <v-card-title primary-title v-if="categorie.active">
+            <h3 class="headline">{{ categorie.name }}</h3>
+        </v-card-title>
+        <v-card-text v-if="categorie.active">
+            <v-data-table hide-headers :items="categorie.goals" hide-actions class="criteriaTable">
+                <template slot="items" slot-scope="props">
+                    <tr v-if="props.item.active">
+                        <th class="oneThirdWidth">{{ props.item.name }}</th>
+                        <div v-for="(criteria) in props.item.criteria" v-bind:key="criteria.name">
+                            <v-layout v-if="criteria.active" class="py-2 mt-2" row>
+                                <v-layout :class="getClass">
+                                    <v-flex>
+                                        <td d-block class="text-xs-left pl-0 xs-5 oneThirdWidth">{{ criteria.name }}</td>
+                                        <v-divider></v-divider>
+                                    </v-flex>
+                                </v-layout>
+                                <template>
 
-                  <!-- <v-spacer></v-spacer> -->
+                                    <!-- <v-spacer></v-spacer> -->
 
-                  <v-btn round class="primary" v-if="evaluations" @click="createScoreDialog(criteria.id)">
-                    <v-badge left color="green">
-                      <span slot="badge">{{evaluations.size}}</span>
-                      <span>scores</span>
-                    </v-badge>
+                                    <v-btn round class="primary" v-if="evaluations" @click="createScoreDialog(criteria.id)">
+                                        <v-badge left color="green">
+                                            <span slot="badge">{{evaluations.size}}</span>
+                                            <span>scores</span>
+                                        </v-badge>
 
-                  </v-btn>
+                                    </v-btn>
 
-                  <!-- <v-spacer></v-spacer> -->
+                                    <!-- <v-spacer></v-spacer> -->
 
-                  <td v-if="evaluating" class="gradeBox">
-                    <gradeboxes :criteriaid="criteria.id" :newEvaluation="newEvaluation" v-on:graded="graded"></gradeboxes>
-                  </td>
+                                    <td v-if="evaluating" class="gradeBox">
+                                        <gradeboxes :criteriaid="criteria.id" :newEvaluation="newEvaluation" v-on:graded="graded"></gradeboxes>
+                                    </td>
+                                </template>
+                            </v-layout>
+                        </div>
+                    </tr>
                 </template>
-              </v-layout>
-            </div>
-          </tr>
-        </template>
-      </v-data-table>
-    </v-card-text>
-  </v-card>
-  <v-dialog width="500" v-model="showScores">
-    <v-card>
-      <v-card-title>
-        <h3>Scores</h3>
-      </v-card-title>
-      <v-card-text v-if="!isEmptyEvalObj(selectedCriteriaScores)">
-        <table class="scores">
-          <tr>
-            <th>Opdracht</th>
-            <th>Score</th>
-          </tr>
-          <tr v-for="(score, key) in selectedCriteriaScores" v-bind:key="key">
-            <td class="score"><strong>{{key}}</strong>:</td>
-            <td class="score">{{score}}</td>
-          </tr>
-        </table>
-      </v-card-text>
-      <v-card-text v-else>
-        <p>Er zijn nog geen evaluaties toegevoegd.</p>
-      </v-card-text>
-      <v-card-actions>
-        <v-btn color="primary" @click="showScores = false">
-          <v-icon>clear</v-icon>Sluit
-        </v-btn>
-      </v-card-actions>
+            </v-data-table>
+        </v-card-text>
     </v-card>
-  </v-dialog>
+    <v-dialog width="500" v-model="showScores">
+        <v-card>
+            <v-card-title>
+                <h3>Scores</h3>
+            </v-card-title>
+            <v-card-text v-if="!isEmptyEvalObj(selectedCriteriaScores)">
+                <table class="scores">
+                    <tr>
+                        <th>Opdracht</th>
+                        <th>Score</th>
+                    </tr>
+                    <tr v-for="(score, key) in selectedCriteriaScores" v-bind:key="key">
+                        <td class="score"><strong>{{key}}</strong>:</td>
+                        <td class="score">{{score}}</td>
+                    </tr>
+                </table>
+            </v-card-text>
+            <v-card-text v-else>
+                <p>Er zijn nog geen evaluaties toegevoegd.</p>
+            </v-card-text>
+            <v-card-actions>
+                <v-btn color="primary" @click="showScores = false">
+                    <v-icon>clear</v-icon>Sluit
+                </v-btn>
+            </v-card-actions>
+        </v-card>
+    </v-dialog>
 </v-layout>
 </template>
 
@@ -77,55 +77,55 @@ import * as grades from "../../constants/grades";
 import _ from "lodash";
 
 export default {
-  name: "moduleList",
-  props: ["module", "evaluating", "evaluations", "newEvaluation"],
-  data() {
-    return {
-      selectedCriteria: null,
-      showScores: false
-    };
-  },
-  methods: {
-    isEmptyEvalObj(obj) {
-      return !obj || _.isEmpty(obj);
+    name: "moduleList",
+    props: ["module", "evaluating", "evaluations", "newEvaluation"],
+    data() {
+        return {
+            selectedCriteria: null,
+            showScores: false
+        };
     },
-    getEvalForCriteria(assignment, id) {
-      let score = "NI";
-      const grade = assignment.find(x => x.criteriaid === id);
-      if (grade) {
-        score = this.getKeyByValue(grades.gradeValues, grade.grade);
-        score = grades.gradeKeys[score];
-      }
-      return score;
+    methods: {
+        isEmptyEvalObj(obj) {
+            return !obj || _.isEmpty(obj);
+        },
+        getEvalForCriteria(assignment, id) {
+            let score = "NI";
+            const grade = assignment.find(x => x.criteriaid === id);
+            if (grade) {
+                score = this.getKeyByValue(grades.gradeValues, grade.grade);
+                score = grades.gradeKeys[score];
+            }
+            return score;
+        },
+        getKeyByValue(object, value) {
+            return Object.keys(object).find(key => object[key] === value);
+        },
+        graded() {
+            console.log(this.newEvaluation);
+            this.$emit("graded");
+        },
+        createScoreDialog(criteriaid) {
+            this.selectedCriteria = criteriaid;
+            this.showScores = true;
+        }
     },
-    getKeyByValue(object, value) {
-      return Object.keys(object).find(key => object[key] === value);
-    },
-    graded() {
-      console.log(this.newEvaluation);
-      this.$emit("graded");
-    },
-    createScoreDialog(criteriaid) {
-      this.selectedCriteria = criteriaid;
-      this.showScores = true;
+    computed: {
+        selectedCriteriaScores() {
+            const scoreObject = {};
+            if (!this.evaluations) {
+                return;
+            }
+            this.evaluations.forEach((value, key) => {
+                let score = this.getEvalForCriteria(value, this.selectedCriteria);
+                scoreObject[key] = score;
+            })
+            return scoreObject;
+        },
+        getClass() {
+            return this.evaluating ? "criteriaTextSm" : "criteriaText";
+        }
     }
-  },
-  computed: {
-    selectedCriteriaScores() {
-      const scoreObject = {};
-      if (!this.evaluations) {
-        return;
-      }
-      this.evaluations.forEach((value, key) => {
-        let score = this.getEvalForCriteria(value, this.selectedCriteria);
-        scoreObject[key] = score;
-      })
-      return scoreObject;
-    },
-    getClass() {
-      return this.evaluating ? "criteriaTextSm" : "criteriaText";
-    }
-  }
 };
 </script>
 
@@ -157,25 +157,25 @@ div.menu__content--autocomplete {
 */
 
 .gradeBox {
-  width: 15%;
-  min-width: 110px;
+    width: 15%;
+    min-width: 110px;
 }
 
 .scores.th {
-  border: none;
-  display: inline-block;
+    border: none;
+    display: inline-block;
 }
 
 .scores {
-  border-spacing: 0;
+    border-spacing: 0;
 }
 
 .score {
-  padding: 1em;
-  border: 1px black solid;
+    padding: 1em;
+    border: 1px black solid;
 }
 
 .oneThirdWidth {
-  width: 30%;
+    width: 30%;
 }
 </style>
